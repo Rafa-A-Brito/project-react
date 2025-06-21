0f674e777384686fb132018e354cf615b30b2293
Rafa-A-Brito <rafabrito1903@gmail.com>
2025-06-20 23:20:05 -0300

    Vamos dar deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -5,10 +5,11 @@ import axios from "axios";
 const { NODE_ENV } = process.env;
 const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
 
+// const URL = "http://localhost:3000/api";
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
 
 export const artistArray = responseArtists.data;
 export const songsArray = responseSongs.data;
 
-console.log(responseArtists);
+// console.log(responseArtists);
