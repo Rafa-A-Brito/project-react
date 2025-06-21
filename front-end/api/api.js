695dda4c48fef20c70482b01e905f9854aa8a830
Rafa-A-Brito <rafabrito1903@gmail.com>
2025-06-20 23:53:49 -0300

    Talvez dê

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,7 +2,7 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
+const NODE_ENV = process.env.NODE_ENV;
 const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
 
 // const URL = "http://localhost:3000/api";
