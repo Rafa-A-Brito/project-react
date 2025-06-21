9c53bfafc59991ebfb829cd22bd28728ad2e742a
Rafa-A-Brito <rafabrito1903@gmail.com>
2025-06-21 00:49:53 -0300

    Ajustes env e process

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,10 +2,10 @@
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+// const { NODE_ENV } = process.env;
+// const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+const URL = "https://project-react-dwjj.onrender.com/api";
 
-// const URL = "http://localhost:3000/api";
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
 
